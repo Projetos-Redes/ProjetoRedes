2854c4c8ddda680f00573b98cf80fc542f9eb215
ThiagoManguinho <tmrs@cesar.school>
2025-04-03 18:10:24 -0300

    Ajeitando prints/comentário entrega 1
    Fiz com João e Gabriel

--- client.py
+++ client.py
@@ -1,64 +1,76 @@
 import socket
 
-# Realizando o handshake
+#Realizando representação do handshake
 def handshake(client_socket):
+    #Escolhendo Modo de Operação
+    while (1):
+        print("Escolha o Modo de Operacao:\n(1) GoBackN\n(2) Repetição Seletiva")
+        escolha = input("Digite sua escolha: ")
+        if escolha == "1":
+            modo = "GoBackN"
+            break
+        elif escolha == "2":
+            modo = "RepetiçãoSeletiva"
+            break
+        else:
+            print("Opição inesistente! Tente novamento\n")
 
-    # 1. Enviando mensagem ao servidor (SYN)
-    modo = "modo_texto"
+    #1. Enviando SYN ao servidor
     tam_max = "1025"
     mensagem = f"SYN|{modo}|{tam_max}"
     client_socket.send(mensagem.encode('utf-8'))
+    print("\nEnviando SYN...")
 
-    # 2. Recebendo a resposta do servidor (SYN-ACK)
+    #2. Recebendo o SYN-ACK do Servidor
     resposta = client_socket.recv(1024).decode('utf-8')
-    print("Resposta do servidor:", resposta)
+    print("Resposta recebida do servidor: ", resposta)
 
-    # Conferindo resposta 
+    #Conferindo resposta
     if resposta.startswith("SYN-ACK") and resposta.split("|")[1] == modo and resposta.split("|")[2] == tam_max:
-        # 3. Manda o (ACK)
+        #3. Enviando ACK ao servidor
         client_socket.send("ACK".encode('utf-8'))
-        print("3° via do handshake feito!")
+        print("Enviando ACK...")
+        print("Handshake feito! Conexão feita com sucesso com o Servidor")
     else:
         print("Erro no handshake")
 
-# Trocar mensagens
-def comunicacao_server(client_socket, message):
 
-    # Enviando mensagem ao servidor
+#Troca de mensagens com o Servidor
+def comunicacao_server(client_socket, message):
+    #Enviando mensagem ao servidor
     msg = f"MSG|{message}"
     client_socket.send(msg.encode('utf-8'))
 
-    # Recebendo resposta do servidor
+    #Recebendo resposta do servidor
     resposta = client_socket.recv(1024).decode('utf-8')
     print("Resposta do servidor:", resposta)
 
 
 def cliente():
-
-    # Conectando ao servidor
+    #Conectando com servidor
     host = '127.0.0.1'  
     port = 8081
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
-    # Realiza o handshake com o servidor
+    #Realiza o handshake com o servidor
     handshake(client_socket)
 
-    # Conversando com o servidor
+    #Troca de mensagem com o Servidor
     while True:
-        # Recebendo mensagem do cliente
-        message = input("Digite sua mensagem (ou 'sair' para encerrar): ")
+        #Recebendo mensagem do cliente
+        message = input("\nDigite sua mensagem (ou 'sair' para encerrar): ")
 
-        # Enviando mensagem para o servidor
+        #Enviando mensagem para o servidor
         comunicacao_server(client_socket, message)
 
-        # Se o usuário digitar 'sair', o loop é interrompido
+        #Se o usuário digitar 'sair', o loop é interrompido
         if message.lower() == 'sair':
             print("Desconectando do servidor...")
             break
 
-    # Fecha a conexão
+    #Fecha a conexão
     client_socket.close()
 
 if __name__ == '__main__':
--- servidor.py
+++ servidor.py
@@ -1,57 +1,50 @@
 import socket
 
-# Realizando o handshake
+#Realizando representação do handshake
 def process_handshake(client_socket):
-
-    # Recebe o handshake inicial do cliente (SYN)
+    #Recebe o HandShake do cliente
     resposta_syn = client_socket.recv(1024).decode('utf-8')
-    print("Recebido handshake:", resposta_syn)
+    print("\nServidor recebendo SYN...")
+    print("SYN recebido: ", resposta_syn)
 
-    # Confere o handshake e separa o modo de operação e o tamanho máximo
+    #Confere o Handshake e separa o modo de operação e o tamanho máximo
     parts = resposta_syn.split('|')
     if parts[0] == "SYN" and len(parts) == 3:
-        modo = parts[1]  # Tipo de operação
-        tam_max = int(parts[2])  # Tamanho máximo do pacote
+        modo = parts[1]  #Tipo de operação
+        tam_max = int(parts[2])  #Tamanho máximo do pacote
 
-        # Envia a resposta para o cliente (SYN-ACK)
+        #Envia SYN-ACK para o cliente
         mensagem_syn_ack = f"SYN-ACK|{modo}|{tam_max}"
         client_socket.send(mensagem_syn_ack.encode('utf-8'))
+        print("Servidor enviando SYN-ACK...")
 
-
-        # Recebendo confirmação (ACK)
+        #Recebendo ACK
         resposta_ack = client_socket.recv(1024).decode('utf-8')
-
+        print("Recebendo ACK...")
         if resposta_ack == "ACK":
-            print("Conexão feito estabelecida - handshake de 3 vias!")
+            print("Conexão estabelecida - handshake de 3 vias!\n")
             return modo, tam_max
         else:
-            print("Erro: ACK não recebido corretamente.")
+            print("Erro: ACK não recebido corretamente.\n")
             return None, None
     else:
-        client_socket.send("Erro no handshake".encode('utf-8'))
+        client_socket.send("Erro no handshake\n".encode('utf-8'))
         return None, None
 
-# Recebendo mesnagens cliente
+#Recebendo mesnagens cliente
 def comunicacao_cliente(client_socket):
-
     while True:
-        # Recebe a mensagem do cliente
+        print("\nAguardando menssagem do Client...")
+        #Recebe a mensagem do cliente
         data = client_socket.recv(1024).decode('utf-8')
-        # Se não houver nada, já quebra
+        #Se estiver vazia, encerra a conexão
         if not data:
             break
-
         print("Mensagem recebida:", data)
 
-        # Se o cliente enviar 'sair', termina a comunicação
-        if data.lower() == 'sair':
-            print("Cliente desconectado...")
-            break
-
-        # Verifica o tipo da mensagem e responde
+        #Verifica o tipo da mensagem e responde
         parts = data.split('|')
         if parts[0] == "MSG":
-            # Mensagem recebida com sucesso, envia a resposta
             resposta = "RESPONSE|Mensagem recebida com sucesso!"
             client_socket.send(resposta.encode('utf-8'))
         elif parts[0] == "NACK":
@@ -60,32 +53,32 @@ def comunicacao_cliente(client_socket):
         else:
             resposta = "NACK|Formato de mensagem inválido"
             client_socket.send(resposta.encode('utf-8'))
+    print("Cliente desconectado!")
 
 def servidor():
-
-    # Definindo o endereço do servidor
+    #Definindo o endereço do servidor
     host = '127.0.0.1'  
     port = 8081
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
 
-    # Esperando mensagem do cliente
+    #Recebendo entrada do usuário
     server_socket.listen(1)
-    print(f"Servidor ouvindo em {host}:{port}...")
+    print(f"Servidor ouvindo em {host}: {port}")
 
-    # Aceitando conexão com o cliente
+    #Aceitando conexão com o cliente
     client_socket, endereco = server_socket.accept()
     print("Conectado cliente no endereço:", endereco)
 
-    # Realiza o handshake
+    #Realiza o handshake
     modo, tam_max = process_handshake(client_socket)
 
     if modo and tam_max:
         print(f"Modo de operação: {modo}, Tamanho máximo de pacote: {tam_max}")
-        # Inicia a troca de mensagens
+        #Inicia a troca de mensagens
         comunicacao_cliente(client_socket)
     
-    # Fecha a conexão
+    #Fecha a conexão
     client_socket.close()
 
 if __name__ == '__main__':
